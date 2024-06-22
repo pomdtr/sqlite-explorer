c606f83ad0613bfffa3c013161c7d5b15cffa36f
pomdtr <achille.lacoin@gmail.com>
2024-06-22 23:16:07 +0200

    fix some broken components in the UI

--- dist/dir.ts
+++ dist/dir.ts
@@ -2,7 +2,7 @@ import {E} from "jsr:@nfnitloop/deno-embedder@1.4.1/embed.ts"
 
 export default E({
   "assets/index-C0_zK9h0.css": () => import("./assets/index-C0_zK9h0.css_.ts"),
-  "assets/index-k2UmuoQu.js": () => import("./assets/index-k2UmuoQu.js_.ts"),
+  "assets/index-LngTDuO-.js": () => import("./assets/index-LngTDuO-.js_.ts"),
+  "favicon.ico": () => import("./favicon.ico_.ts"),
   "index.html": () => import("./index.html_.ts"),
-  "vite.svg": () => import("./vite.svg_.ts"),
 })
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import { Studio } from "@libsqlstudio/gui";
 import SqliteDriver from "./driver.ts";
+import { useLocalStorage } from "@uidotdev/usehooks";
 import "@libsqlstudio/gui/css";
 
 export default function App() {
+  const [theme, saveTheme] = useLocalStorage<"light" | "dark">(
+    "theme",
+    "light",
+  );
+
   return (
     <Studio
       driver={new SqliteDriver()}
-      name="Turso Connection"
-      theme="light"
+      name="SQLite Explorer"
+      theme={theme}
+      onThemeChange={saveTheme}
       color="blue"
     />
   );
